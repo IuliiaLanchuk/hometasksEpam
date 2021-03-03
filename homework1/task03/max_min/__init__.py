b09a9195f452221b0e72048630fd7a9475631d8b
IuliiaLanchuk <lanchuk.iuliia@gmail.com>
2021-03-03 21:48:36 +0300

    Homework1 task3

---
+++ homework1/task03/max_min/__init__.py
@@ -0,0 +1,5 @@
+"""
+This is __init__.py docstring.
+
+Some text to pass flake8.
+"""
---
+++ homework1/task03/tests/test_max_min.py
@@ -0,0 +1,24 @@
+"""
+This is test_max_min.py docstring.
+
+Here are tests for find_maximum_and_minimum function
+"""
+
+
+import pytest
+from max_min.task03 import find_maximum_and_minimum
+
+
+@pytest.mark.parametrize(
+    ("file_name", "expected_result"),
+    [
+        ("data_1.txt", (-4, 2030)),
+        ("data_2.txt", (5, 5)),
+        ("data_3.txt", (-8, 89)),
+        ("data_4.txt", (9223372036854775807, 0)),
+    ],
+)
+def test_max_min(file_name: str, expected_result: tuple[int, int]):
+    actual_result = find_maximum_and_minimum(file_name)
+
+    assert actual_result == expected_result
