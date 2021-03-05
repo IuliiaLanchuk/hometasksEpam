420bcf07e3a5755256d54cb8a3f2629dc3f0de0c
IuliiaLanchuk <lanchuk.iuliia@gmail.com>
2021-03-05 17:15:30 +0300

    Hw2 task1 pytest file-path changed

--- homework2/task01/tests/test_file_operations.py
+++ homework2/task01/tests/test_file_operations.py
@@ -6,9 +6,11 @@ from project1.task1 import (
     get_rarest_char,
 )
 
+FILE_PATH = "homework2/task01/tests/data.txt"
+
 
 def test_get_longest_diverse_words():
-    actual_result = get_longest_diverse_words("data.txt")
+    actual_result = get_longest_diverse_words(FILE_PATH)
     expected_result = [
         "unmißverständliche",
         "Kollektivschuldiger",
@@ -25,16 +27,16 @@ def test_get_longest_diverse_words():
 
 
 def test_get_rarest_char():
-    assert get_rarest_char("data.txt") == "X"
+    assert get_rarest_char(FILE_PATH) == "X"
 
 
 def test_count_punctuation_chars():
-    assert count_punctuation_chars("data.txt") == 5475
+    assert count_punctuation_chars(FILE_PATH) == 5475
 
 
 def test_count_non_ascii_chars():
-    assert count_non_ascii_chars("data.txt") == 2802
+    assert count_non_ascii_chars(FILE_PATH) == 2802
 
 
 def test_get_most_common_non_ascii_char():
-    assert get_most_common_non_ascii_char("data.txt") == "ä"
+    assert get_most_common_non_ascii_char(FILE_PATH) == "ä"
