6e19fbeb2447ad50a21038f3150c2c29847771e8
IuliiaLanchuk <lanchuk.iuliia@gmail.com>
2021-03-06 13:03:47 +0300

    Hw1 task3 remove docstring __init__py and simplify tests

--- homework1/task04/sum_of_four/task04.py
+++ homework1/task04/sum_of_four/task04.py
@@ -5,7 +5,6 @@ Given four lists A, B, C, D of integer values,
 compute how many tuples (i, j, k, l) there are such that A[i] + B[j] + C[k] + D[l] is zero.
 We guarantee, that all A, B, C, D have same length of N where 0 <= N <= 1000.
 """
-
 from typing import List
 
 
@@ -18,7 +17,6 @@ def check_sum_of_four(a: List[int], b: List[int], c: List[int], d: List[int]) ->
         for j in range(len(c)):
             ab.append(a[i] + b[j])
             cd.append(c[i] + d[j])
-
     for i in ab:
         for j in cd:
             if i == 0 - j:
--- homework1/task04/tests/test_sum_of_four.py
+++ homework1/task04/tests/test_sum_of_four.py
@@ -1,9 +1,3 @@
-"""
-This is test_sum_of_four.py docstring.
-
-Here are tests for check_sum_of_four function
-"""
-
 import pytest
 from sum_of_four.task04 import check_sum_of_four
 
@@ -11,38 +5,12 @@ from sum_of_four.task04 import check_sum_of_four
 @pytest.mark.parametrize(
     ("a", "b", "c", "d", "expected_result"),
     [
-        (
-            [1, -2, 3, 4, -5],
-            [6, 7, 2, 9, 5],
-            [-10, 12, -13, 14, 15],
-            [16, 17, 13, 19, 20],
-            4,
-        ),
-        (
-            [1, -2, 3, 4, 5],
-            [6, 7, 2, 9, 5],
-            [-10, 12, -13, 14, 15],
-            [16, 17, 13, 19, 20],
-            1,
-        ),
-        (
-            [1, 2, 3, 4, 5],
-            [6, 7, 2, 9, 5],
-            [10, 12, 13, 14, 15],
-            [16, 17, 13, 19, 20],
-            0,
-        ),
-        (
-            [1, 2, 3, 4, 5, 6, 7, 8, 9],
-            [11, 12, 13, 14, 15, 16, 17, 18, 19],
-            [-1, -2, -3, -4, -5, -6, -7, -8, -9],
-            [-11, -12, -13, -14, -15, -16, -17, -18, -19],
-            489,
-        ),
-        ([1], [1], [-1], [-1], 1),
+        ([-2, -3], [-3, 8], [3, 8], [2, 2], 2),
+        ([], [], [], [], 0),
+        ([0], [0], [0], [0], 1),
     ],
 )
 def test_sum_of_four(
-    a: list[int], b: list[int], c: list[int], d: list[int], expected_result: tuple
+    a: list[int], b: list[int], c: list[int], d: list[int], expected_result: int
 ):
     assert check_sum_of_four(a, b, c, d) == expected_result
