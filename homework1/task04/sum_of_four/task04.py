2b6b399f4756da3afd84684b09152b4dac9dbbc9
IuliiaLanchuk <lanchuk.iuliia@gmail.com>
2021-03-09 12:44:03 +0300

    Hw1 task4 - new code implementation with default dict

--- homework1/task04/sum_of_four/task04.py
+++ homework1/task04/sum_of_four/task04.py
@@ -5,20 +5,22 @@ Given four lists A, B, C, D of integer values,
 compute how many tuples (i, j, k, l) there are such that A[i] + B[j] + C[k] + D[l] is zero.
 We guarantee, that all A, B, C, D have same length of N where 0 <= N <= 1000.
 """
+from collections import defaultdict
 from typing import List
 
 
 def check_sum_of_four(a: List[int], b: List[int], c: List[int], d: List[int]) -> int:
     """Return the amount of tuples where sum of elements is zero."""
-    ab = []
-    cd = []
+    ab_sums = defaultdict(int)
     counter = 0
-    for i in range(len(a)):
-        for j in range(len(c)):
-            ab.append(a[i] + b[j])
-            cd.append(c[i] + d[j])
-    for i in ab:
-        for j in cd:
-            if i == 0 - j:
-                counter += 1
+    for i in a:
+        for j in b:
+            ab = -i - j
+            ab_sums[ab] += 1
+
+    for i in c:
+        for j in d:
+            cd = i + j
+            if cd in ab_sums:
+                counter += ab_sums[cd]
     return counter
