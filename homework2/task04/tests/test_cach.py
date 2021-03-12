b02cc8103646efd2f2cc98dac653d20ad49f7cfa
IuliiaLanchuk <lanchuk.iuliia@gmail.com>
2021-03-12 13:32:32 +0300

    Hw2 task4 changed tests with Mock and added test_cache_sum

--- homework2/task04/tests/test_cach.py
+++ homework2/task04/tests/test_cach.py
@@ -1,7 +1,5 @@
-from typing import Callable
 from unittest.mock import Mock, call
 
-import pytest
 from project4.task4 import cache
 
 
@@ -10,44 +8,31 @@ def function(a: int, b: int) -> int:
     return pow(pow(a, b), 2)
 
 
-@pytest.mark.parametrize(
-    "func",
-    [
-        sum,
-        pow,
-        function,
-    ],
-)
-def test_cache_same_values(func: Callable):
+def test_cache_same_values_func_called_ones():
     some = [2, 4, 7]
-    mock_func = Mock(func)
+    mock_func = Mock()
     cache_calc_sum_of_values = cache(mock_func)
 
     cache_calc_sum_of_values(*some)
     cache_calc_sum_of_values(*some)
 
     mock_func.assert_called_once()
-
-    actual_result = mock_func.mock_calls
-    assert actual_result == [call((2, 4, 7))]
+    assert mock_func.mock_calls == [call((2, 4, 7))]
 
 
-@pytest.mark.parametrize(
-    "func",
-    [
-        sum,
-        pow,
-        function,
-    ],
-)
-def test_cache_different_values(func: Callable):
-    func = Mock(func)
+def test_cache_different_values_func_called_several_times():
+    func = Mock()
     cache_calc_sum_of_values = cache(func)
 
     cache_calc_sum_of_values(2, 4, 7)
     cache_calc_sum_of_values(2, 4, 8)
 
     assert func.call_count == 2
+    assert func.mock_calls == [call((2, 4, 7)), call((2, 4, 8))]
 
-    actual_result = func.mock_calls
-    assert actual_result == [call((2, 4, 7)), call((2, 4, 8))]
+
+def test_cache_sum():
+    some1 = [1, 2]
+    some2 = [1, 3]
+    cache_calc_sum_of_values = cache(sum)
+    assert cache_calc_sum_of_values(*some1) != cache_calc_sum_of_values(*some2)
