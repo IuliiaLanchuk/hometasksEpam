13742769a93051953df36dabb0621305d87ffbca
IuliiaLanchuk <lanchuk.iuliia@gmail.com>
2021-03-17 22:41:49 +0300

    Hw4 task5 - add fizzbuzz function in terms of generator implementation and tests

---
+++ homework4/task5/project5_optional/task5_optional.py
@@ -0,0 +1,37 @@
+"""
+This task is optional.
+
+Write a generator that takes a number N as an input
+and returns a generator that yields N FizzBuzz numbers*
+Don't use any ifs, you can find an approach for the implementation in this video**.
+
+
+Definition of done:
+ - function is created
+ - function is properly formatted
+ - function has tests
+
+
+#>>> list(fizzbuzz(5))
+["1", "2", "fizz", "4", "buzz"]
+
+* https://en.wikipedia.org/wiki/Fizz_buzz
+** https://www.youtube.com/watch?v=NSzsYWckGd4
+"""
+from typing import Generator
+
+
+def fizzbuzz(n: int) -> Generator:
+    def fizz_buzz_generation(x: int) -> (int, str):
+        return (
+            "fizz buzz" * (x % 15 == 0)
+            or "fizz" * (x % 3 == 0)
+            or "buzz" * (x % 5 == 0)
+            or str(x)
+        )
+
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("Input error. Required positive number")
+    else:
+        for i in map(fizz_buzz_generation, range(1, n + 1)):
+            yield i
