6e7923e60bc1f9642434aa516fc828e4008a2c4e
IuliiaLanchuk <lanchuk.iuliia@gmail.com>
2021-03-19 12:47:38 +0300

    Hw3 task3 - add new test cases, new test names

--- homework3/task03/project_filter/filter.py
+++ homework3/task03/project_filter/filter.py
@@ -3,6 +3,7 @@
 The task is to write a code that generates data filtering object from a list of keyword parameters.
 There are multiple bugs in this code. Find them all and write tests for faulty cases.
 """
+from functools import partial
 
 
 class Filter:
@@ -15,18 +16,19 @@ class Filter:
         self.functions = functions
 
     def apply(self, data):
-        return [item for item in data if all(i(item) for i in self.functions)]
+        return [
+            item for item in data if all(function(item) for function in self.functions)
+        ]
 
 
 def make_filter(**keywords):
-    """
-    Generate filter object for specified keywords
-    """
+    """Generate filter object for specified keywords."""
     filter_funcs = []
     for key, value in keywords.items():
 
-        def keyword_filter_func(data, key_=key, value_=value):
-            return data[key_] == value_
+        def keyword_filter_func(data, key_, value_):
+            if key_ in data:
+                return data[key_] == value_
 
-        filter_funcs.append(keyword_filter_func)
+        filter_funcs.append(partial(keyword_filter_func, key_=key, value_=value))
     return Filter(*filter_funcs)
--- homework3/task03/tests/test_filter.py
+++ homework3/task03/tests/test_filter.py
@@ -1,39 +1,130 @@
-from typing import Any, Dict, List
-
-import pytest
 from project_filter.filter import Filter, make_filter
 
-sample_data = [
-    {
-        "name": "Bill",
-        "last_name": "Gilbert",
-        "occupation": "was here",
-        "type": "person",
-    },
-    {
-        "is_dead": True,
-        "kind": "parrot",
-        "type": "bird",
-        "name": "polly",
-    },
-]
 
-
-@pytest.mark.parametrize(
-    ("data", "expected_result"),
-    [
-        ({"name": "polly"}, [sample_data[1]]),
-        ({"name": "Bill", "occupation": "was here"}, [sample_data[0]]),
-        ({"name": "Iuliia"}, []),
-    ],
-)
-def test_make_filter(data: Dict[Any, Any], expected_result: List[Any]):
-    assert make_filter(**data).apply(sample_data) == expected_result
-
-
-def test_apply():
+def test_apply_positive_even():
     positive_even = Filter(
         lambda a: a % 2 == 0, lambda a: a > 0, lambda a: isinstance(a, int)
     )
     assert positive_even.apply(range(10)) == [2, 4, 6, 8]
-    assert positive_even.apply(range(10)) != [0, 2, 4, 6]
+
+
+def test_apply_sum_with_7_is_zero():
+    sum_with_7_is_zero = Filter(lambda a: a != 0, lambda a: a + 7 == 0)
+    assert sum_with_7_is_zero.apply([-7, 13, 6.8, 9, 0]) == [-7]
+
+
+def test_apply_no_data_to_filter():
+    assert Filter().apply([]) == []
+
+
+def test_apply_no_filtering_functions():
+    assert Filter().apply(range(5)) == [0, 1, 2, 3, 4]
+
+
+def test_one_key_value_pair_repeats_in_different_team_members():
+    team = [
+        {
+            "name": "Iuliia",
+            "last_name": "Lanchuk",
+            "hobby": "swimming",
+            "type": "human",
+        },
+        {
+            "name": "Barsik",
+            "color": "black",
+            "occupation": "was here",
+            "type": "cat",
+        },
+        {
+            "name": "Evgenii",
+            "last_name": "Mukhailov",
+            "hobby": "swimming",
+            "type": "human",
+        },
+    ]
+    assert make_filter(**{"type": "human"}).apply(team) == [
+        {
+            "name": "Iuliia",
+            "last_name": "Lanchuk",
+            "hobby": "swimming",
+            "type": "human",
+        },
+        {
+            "name": "Evgenii",
+            "last_name": "Mukhailov",
+            "hobby": "swimming",
+            "type": "human",
+        },
+    ]
+
+
+def test_no_such_key():
+    team = [
+        {
+            "name": "Iuliia",
+            "last_name": "Lanchuk",
+            "hobby": "swimming",
+            "type": "human",
+        },
+        {
+            "name": "Barsik",
+            "color": "black",
+            "occupation": "was here",
+            "type": "cat",
+        },
+        {
+            "name": "Evgenii",
+            "last_name": "Mukhailov",
+            "hobby": "swimming",
+            "type": "human",
+        },
+    ]
+    assert make_filter(**{"hi": "human"}).apply(team) == []
+
+
+def test_no_such_value():
+    team = [
+        {
+            "name": "Iuliia",
+            "last_name": "Lanchuk",
+            "hobby": "swimming",
+            "type": "human",
+        },
+        {
+            "name": "Barsik",
+            "color": "black",
+            "occupation": "was here",
+            "type": "cat",
+        },
+        {
+            "name": "Evgenii",
+            "last_name": "Mukhailov",
+            "hobby": "swimming",
+            "type": "human",
+        },
+    ]
+    assert make_filter(**{"hobby": "dancing"}).apply(team) == []
+
+
+def test_two_key_value_pairs_from_different_team_members():
+    team = [
+        {
+            "name": "Iuliia",
+            "last_name": "Lanchuk",
+            "hobby": "swimming",
+            "type": "human",
+        },
+        {
+            "name": "Barsik",
+            "color": "black",
+            "occupation": "was here",
+            "type": "cat",
+        },
+        {
+            "name": "Evgenii",
+            "last_name": "Mukhailov",
+            "hobby": "swimming",
+            "type": "human",
+        },
+    ]
+    assert make_filter(**{"name": "Iuliia", "last_name": "Mukhailov"}).apply(team) == []
