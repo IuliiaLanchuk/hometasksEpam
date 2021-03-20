28ff63cf45cd4e810cba4a1cfda2359f42570a6b
IuliiaLanchuk <lanchuk.iuliia@gmail.com>
2021-03-20 11:58:47 +0300

    Hw4 task2 - inline url

--- homework4/task2/tests/test_mock_input.py
+++ homework4/task2/tests/test_mock_input.py
@@ -3,11 +3,9 @@ from unittest.mock import Mock, patch
 import pytest
 from project2_mock_input.task2_mock_input import count_dots_on_i
 
-url = "https://example.com/"
-
 
 def test_count_dots_on_i():
-    assert count_dots_on_i(url) == 59
+    assert count_dots_on_i("https://example.com/") == 59
 
 
 @patch("requests.get")
@@ -21,6 +19,6 @@ def test_count_dots_on_i_with_mock_positive(mock):
 @patch("requests.get", side_effect=Exception("Unknown url"))
 def test_count_dots_on_i_exception(mock):
     with pytest.raises(ValueError, match="Unreachable"):
-        count_dots_on_i(url)
+        count_dots_on_i("https://example.com/")
 
-    mock.assert_called_with(url)
+    mock.assert_called_with("https://example.com/")
