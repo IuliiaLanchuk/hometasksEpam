658e5e2daa2051e538eda3c3201e0d0bb8ebac39
IuliiaLanchuk <lanchuk.iuliia@gmail.com>
2021-03-26 13:48:56 +0300

    hw6 task1 - instances_counter decorator - code and tests

---
+++ homework6/task1_oop2_exceptions/oop2_exceptions/oop2_exc.py
@@ -0,0 +1,36 @@
+"""
+Написать декоратор instances_counter, который применяется к любому классу
+и добавляет ему 2 метода:
+get_created_instances - возвращает количество созданых экземпляров класса
+reset_instances_counter - сбросить счетчик экземпляров,
+возвращает значение до сброса
+Имя декоратора и методов не менять
+Ниже пример использования
+"""
+
+
+def instances_counter(cls):
+    """Is a decorator that implemented for each class that adds two methods to sent class."""
+
+    class ClassHelper(cls):
+        """Adds two additional methods to parent class to count created instances and reset this counter."""
+
+        instances_amount = 0
+
+        def __init__(self, *args):
+            super().__init__(*args)
+            self.__class__.instances_amount += 1
+
+        @classmethod
+        def get_created_instances(cls) -> int:
+            """Return amount of instances created in sent class."""
+            return cls.instances_amount
+
+        @classmethod
+        def reset_instances_counter(cls) -> int:
+            """Return amount of instances created in sent class before amount reset."""
+            to_return = cls.get_created_instances()
+            cls.instances_amount = 0
+            return to_return
+
+    return ClassHelper
---
+++ homework6/task1_oop2_exceptions/tests/test_oop2_exc.py
@@ -0,0 +1,44 @@
+from oop2_exceptions.oop2_exc import instances_counter
+
+
+@instances_counter
+class User:
+    pass
+
+
+@instances_counter
+class Automobile:
+    color = "black"
+
+    def __init__(self, year):
+        self.year = year
+
+    def some_method(self):
+        return self.year
+
+
+def test_user_class_create_instances_and_reset_instance():
+    assert User.get_created_instances() == 0
+    user, _, _ = User(), User(), User()
+    assert user.get_created_instances() == 3
+    assert user.reset_instances_counter() == 3
+
+
+def test_user_create_instances_reset_and_then_check_instances_amount():
+    user, _ = User(), User()
+    assert user.reset_instances_counter() == 2
+    assert user.get_created_instances() == 0
+
+
+def test_automobile_class_create_and_reset_instance():
+    assert Automobile.get_created_instances() == 0
+    automobile = Automobile(2005)
+    assert automobile.get_created_instances() == 1
+    assert automobile.reset_instances_counter() == 1
+
+
+def test_automobile_class_attributes_and_methods_do_not_change():
+    opel_astra = Automobile(1998)
+    assert opel_astra.color == "black"
+    assert opel_astra.some_method() == 1998
+    assert opel_astra.get_created_instances() == 1
