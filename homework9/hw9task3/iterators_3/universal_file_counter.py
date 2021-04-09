56f3b735b33d916fb8e8f1706c5a11a5cf079abb
IuliiaLanchuk <lanchuk.iuliia@gmail.com>
2021-04-09 16:38:53 +0300

    change code to decrease amount of lines, change way of file_path generation

--- homework9/hw9task3/iterators_3/universal_file_counter.py
+++ homework9/hw9task3/iterators_3/universal_file_counter.py
@@ -20,13 +20,13 @@ def universal_file_counter(
     amount_of_tokens_or_lines = 0
     all_files = [file for file in os.listdir(dir_path) if file.endswith(file_extension)]
     for file_name in all_files:
-        file_path = str(dir_path) + "/" + file_name
+        file_path = f"{dir_path}/{file_name}"
         if getsize(file_path) != 0:
             with open(file_path, "r") as file:
-                if not tokenizer:
-                    amount_of_tokens_or_lines += sum(1 for line in file)
-                else:
-                    all_data = file.read()
-                    amount_of_tokens_or_lines += sum(1 for item in tokenizer(all_data))
+                amount_of_tokens_or_lines += (
+                    sum(len(token) for token in map(tokenizer, file))
+                    if tokenizer
+                    else sum(1 for line in file)
+                )
 
     return amount_of_tokens_or_lines
--- homework9/hw9task3/tests/test_universal_file_counter.py
+++ homework9/hw9task3/tests/test_universal_file_counter.py
@@ -7,12 +7,7 @@ path = os.path.dirname(__file__)
 
 def test_universal_file_counter_with_tokenizer_data_split_by_space():
     assert universal_file_counter(path, "txt", str.split) == 11
-    assert universal_file_counter(path, "txt", lambda data: data.split()) == 11
 
 
 def test_universal_file_counter_without_tokenizer():
     assert universal_file_counter(path, "txt") == 8
-
-
-def test_universal_file_counter_with_tokenizer_data_split_by_symbol():
-    assert universal_file_counter(path, "txt", lambda data: data.split("c")) == 5
