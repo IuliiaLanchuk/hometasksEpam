7a0320ef38ff6e5d523c43a0b1f9d168a8e2d72f
IuliiaLanchuk <lanchuk.iuliia@gmail.com>
2021-04-09 14:38:52 +0300

    delete print() from tests, add xfail test

--- homework9/hw9task2/tests/test_context_manager.py
+++ homework9/hw9task2/tests/test_context_manager.py
@@ -4,37 +4,44 @@ from iterators_2.context_manager import SuppressorClass, suppressor_generator
 
 def test_suppressor_as_generator_index_error_positive():
     with suppressor_generator(IndexError):
-        print(["a", "b", "c"][4])
+        ["a", "b", "c"][4]
 
 
-def test_suppressor_as_generator_index_error_negative():
+def test_suppressor_as_generator_suppress_value_error_but_we_have_index_error_positive():
     with pytest.raises(IndexError):
         with suppressor_generator(ValueError):
-            print(["a", "b", "c"][4])
+            ["a", "b", "c"][4]
+
+
+@pytest.mark.xfail
+def test_suppressor_as_generator_suppress_index_error_and_we_have_also_index_error_negative():
+    with pytest.raises(IndexError):
+        with suppressor_generator(IndexError):
+            [][0]
 
 
 def test_suppressor_as_class_index_error_positive():
     with SuppressorClass(IndexError):
-        print([][2])
+        [][2]
 
 
 def test_suppressor_as_class_index_error_negative():
     with pytest.raises(IndexError):
         with SuppressorClass(ValueError):
-            print([][2])
+            [][2]
 
 
 def test_suppressor_as_class_zero_division_error_positive():
     with SuppressorClass(ZeroDivisionError):
-        print(1 / 0)
+        1 / 0
 
 
 def test_suppressor_as_class_zero_division_error_negative():
     with pytest.raises(ZeroDivisionError):
         with SuppressorClass(ValueError):
-            print(1 / 0)
+            1 / 0
 
 
 def test_suppressor_as_generator_value_error_positive():
     with suppressor_generator(ValueError):
-        print(int("false"))
+        int("false")
