0527edbf12735b458a3acebd468e8701fec9263b
IuliiaLanchuk <lanchuk.iuliia@gmail.com>
2021-04-10 16:46:35 +0300

    delete xfailed test

--- homework9/hw9task2/tests/test_context_manager.py
+++ homework9/hw9task2/tests/test_context_manager.py
@@ -4,20 +4,13 @@ from iterators_2.context_manager import SuppressorClass, suppressor_generator
 
 def test_suppressor_as_generator_index_error_positive():
     with suppressor_generator(IndexError):
-        ["a", "b", "c"][4]
+        raise IndexError()
 
 
 def test_suppressor_as_generator_suppress_value_error_but_we_have_index_error_positive():
     with pytest.raises(IndexError):
         with suppressor_generator(ValueError):
             ["a", "b", "c"][4]
-
-
-@pytest.mark.xfail
-def test_suppressor_as_generator_suppress_index_error_and_we_have_also_index_error_negative():
-    with pytest.raises(IndexError):
-        with suppressor_generator(IndexError):
-            [][0]
 
 
 def test_suppressor_as_class_index_error_positive():
