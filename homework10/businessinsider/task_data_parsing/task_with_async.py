7d258c02d329fdc11444ac57a7d9798680171585
IuliiaLanchuk <lanchuk.iuliia@gmail.com>
2021-04-18 12:17:02 +0300

    use one session, change get_value_with_reg_exp func from findall to search, file.write(json.dumps) to json.dump

--- homework10/businessinsider/task_data_parsing/task_with_async.py
+++ homework10/businessinsider/task_data_parsing/task_with_async.py
@@ -23,17 +23,17 @@ import asyncio
 import datetime
 import json
 import re
-from typing import Dict, List, Union
+from typing import Dict, List, Optional
 
 import aiohttp
 from bs4 import BeautifulSoup
 
 
-async def get_current_dollar_value() -> float:
+async def get_current_dollar_value(session) -> float:
     today = datetime.date.today().strftime("%d/%m/%Y")
     central_bank_url = f"http://www.cbr.ru/scripts/XML_daily.asp?date_req={today}"
     dollar_value = (
-        BeautifulSoup(await get_html_content(central_bank_url), "lxml")
+        BeautifulSoup(await get_html_content(session, central_bank_url), "lxml")
         .find("valute", id="R01235")
         .find("value")
         .text.replace(",", ".")
@@ -41,14 +41,13 @@ async def get_current_dollar_value() -> float:
     return float(dollar_value)
 
 
-def get_value_with_reg_exp(value, soup) -> Union[float, None]:
-    pattern = r"([0-9]*\.[0-9]*)\r\n.*" + value
-    request = re.findall(pattern, str(soup))
-    if len(request) != 0:
-        return float(request[0].replace(",", ""))
+def get_value_with_reg_exp(parameter, soup) -> Optional[float]:
+    pattern = r"([0-9]*\.[0-9]*)\r\n.*" + parameter
+    search_value = re.search(pattern, str(soup))
+    return float(search_value.group(1).replace(",", "")) if search_value else None
 
 
-def get_potential_profit(soup) -> Union[float, None]:
+def get_potential_profit(soup) -> Optional[float]:
     low52week_value = get_value_with_reg_exp(
         "52 Week Low", soup.find("div", {"class": "snapshot"})
     )
@@ -59,16 +58,15 @@ def get_potential_profit(soup) -> Union[float, None]:
         return round((high52week_value - low52week_value) / low52week_value * 100, 2)
 
 
-async def get_html_content(url_: str) -> str:
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url_) as response:
-            return await response.text()
+async def get_html_content(session, url_: str) -> str:
+    async with session.get(url_) as response:
+        return await response.text()
 
 
-async def fetch_company_name_href_and_growth(url: str) -> List[Dict]:
+async def fetch_company_name_href_and_growth(url: str, session) -> List[Dict]:
     company_name_href_growth = []
     data_from_all_companies_page = (
-        BeautifulSoup(await get_html_content(url), "lxml")
+        BeautifulSoup(await get_html_content(session, url), "lxml")
         .find_all("table")[1]
         .find_all("tr")[1:]
     )
@@ -85,10 +83,12 @@ async def fetch_company_name_href_and_growth(url: str) -> List[Dict]:
     return company_name_href_growth
 
 
-async def get_company_full_info(company_name_href_growth, current_dollar_value) -> Dict:
+async def get_company_full_info(
+    session, company_name_href_growth, current_dollar_value
+) -> Dict:
     base_url = "https://markets.businessinsider.com"
     company_url = base_url + company_name_href_growth["href"]
-    soup = BeautifulSoup(await get_html_content(company_url), "lxml")
+    soup = BeautifulSoup(await get_html_content(session, company_url), "lxml")
     price_in_dollars = soup.find(class_="price-section__current-value").text.replace(
         ",", ""
     )
@@ -102,6 +102,7 @@ async def get_company_full_info(company_name_href_growth, current_dollar_value) 
         "growth": company_name_href_growth["growth"],
         "potential_profit": get_potential_profit(soup),
     }
+    print(one_company_full_data)
     return one_company_full_data
 
 
@@ -140,28 +141,31 @@ def save_data_in_json(sorted_data: List[Dict], description: str, key: str):
     ]
     json_file_name = "ten_companies_with_" + description + "_" + key + ".json"
     with open(json_file_name, "w") as file:
-        file.write(json.dumps(data_to_json_file, indent=4))
+        json.dump(data_to_json_file, file, indent=4)
 
 
 async def main():
     url_start = "https://markets.businessinsider.com/index/components/s&p_500?p="
     ten_pages = [url_start + str(value) for value in range(1, 11)]
-    tasks_1 = [
-        asyncio.create_task(fetch_company_name_href_and_growth(url))
-        for url in ten_pages
-    ]
-    data_from_all_pages = await asyncio.gather(*tasks_1)
-    current_dollar_value = await get_current_dollar_value()
-    tasks_2 = [
-        asyncio.create_task(get_company_full_info(company, current_dollar_value))
-        for data_from_one_page in data_from_all_pages
-        for company in data_from_one_page
-    ]
-    return await asyncio.gather(*tasks_2)
+    async with aiohttp.ClientSession() as session:
+        tasks_1 = [
+            asyncio.create_task(fetch_company_name_href_and_growth(url, session))
+            for url in ten_pages
+        ]
+        data_from_all_pages = await asyncio.gather(*tasks_1)
+        current_dollar_value = await get_current_dollar_value(session)
+        tasks_2 = [
+            asyncio.create_task(
+                get_company_full_info(session, company, current_dollar_value)
+            )
+            for data_from_one_page in data_from_all_pages
+            for company in data_from_one_page
+        ]
+        return await asyncio.gather(*tasks_2)
 
 
 if __name__ == "__main__":
-    companies_data = asyncio.run(main())
+    companies_data = asyncio.get_event_loop().run_until_complete(main())
     ten_most_expensive(companies_data)
     ten_lowest_p_e_ratio(companies_data)
     ten_largest_growth(companies_data)
