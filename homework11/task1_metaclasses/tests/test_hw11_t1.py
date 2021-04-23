a9a09e3ff1d43570e76e4c00bd4188261333da11
IuliiaLanchuk <lanchuk.iuliia@gmail.com>
2021-04-23 14:15:22 +0300

    change tests, split into two and add third one

--- homework11/task1_metaclasses/tests/test_hw11_t1.py
+++ homework11/task1_metaclasses/tests/test_hw11_t1.py
@@ -1,16 +1,25 @@
 from project_hw11_task1.task1 import SimplifiedEnum
 
 
-class ColorsEnum(metaclass=SimplifiedEnum):
-    __keys = ("RED", "BLUE", "ORANGE", "BLACK")
+def test_color_attributes_are_accessible_using_metaclass():
+    class ColorsEnum(metaclass=SimplifiedEnum):
+        __keys = ("RED", "BLUE", "ORANGE", "BLACK")
 
-
-class SizesEnum(metaclass=SimplifiedEnum):
-    __keys = ("XL", "L", "M", "S", "XS")
-
-
-def test_attributes_are_accessible():
     a = ColorsEnum()
     assert ColorsEnum.RED == "RED"
     assert a.RED == "RED"
-    assert SizesEnum.XL == "XL"
+
+
+def test_size_attributes_are_accessible_using_metaclass():
+    class SizesEnum(metaclass=SimplifiedEnum):
+        __keys = ("XL", "L", "M", "S", "XS")
+
+    assert SizesEnum.M == "M"
+    assert hasattr(SizesEnum, "XL")
+
+
+def test_attributes_are_inaccessible_without_using_metaclass():
+    class GeomFiguresEnum:
+        __keys = ("DOT", "SEGMENT", "TRIANGLE", "SQUARE", "PENTAGON", "HEXAGON")
+
+    assert not hasattr(GeomFiguresEnum, "TRIANGLE")
