e5558c29aeabb3679607886538b35c232c2c0d5c
IuliiaLanchuk <lanchuk.iuliia@gmail.com>
2021-04-25 11:28:48 +0300

    change test names and content

--- homework11/task1_metaclasses/tests/test_hw11_t1.py
+++ homework11/task1_metaclasses/tests/test_hw11_t1.py
@@ -1,24 +1,33 @@
+import pytest
 from project_hw11_task1.task1 import SimplifiedEnum
 
 
-def test_color_attributes_are_accessible_using_metaclass():
+def test_colors_enum():
     class ColorsEnum(metaclass=SimplifiedEnum):
         __keys = ("RED", "BLUE", "ORANGE", "BLACK")
 
     a = ColorsEnum()
-    assert ColorsEnum.RED == "RED"
     assert a.RED == "RED"
+    assert a.BLUE == "BLUE"
+    assert a.ORANGE == "ORANGE"
+    assert a.BLACK == "BLACK"
+    with pytest.raises(AttributeError):
+        a.GREEN
 
 
-def test_size_attributes_are_accessible_using_metaclass():
+def test_sizes_enum():
     class SizesEnum(metaclass=SimplifiedEnum):
         __keys = ("XL", "L", "M", "S", "XS")
 
-    assert SizesEnum.M == "M"
     assert hasattr(SizesEnum, "XL")
+    assert hasattr(SizesEnum, "L")
+    assert hasattr(SizesEnum, "M")
+    assert hasattr(SizesEnum, "S")
+    assert hasattr(SizesEnum, "XS")
+    assert not hasattr(SizesEnum, "XXS")
 
 
-def test_attributes_are_inaccessible_without_using_metaclass():
+def test_enum_without_using_metaclass():
     class GeomFiguresEnum:
         __keys = ("DOT", "SEGMENT", "TRIANGLE", "SQUARE", "PENTAGON", "HEXAGON")
 
